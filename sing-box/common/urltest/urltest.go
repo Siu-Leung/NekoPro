6972e38e406be8c7bdcd437ff5e391fd4701a81f
Hermes <hermes@agents>
2026-08-23 06:04:13 +0800

    fix: urltest.go 补回缺失的 sing/common import (1.13 升级遗漏)

--- sing-box/common/urltest/urltest.go
+++ sing-box/common/urltest/urltest.go
@@ -11,6 +11,7 @@ import (
 
 	"github.com/sagernet/sing-box/adapter"
 	C "github.com/sagernet/sing-box/constant"
+	"github.com/sagernet/sing/common"
 	M "github.com/sagernet/sing/common/metadata"
 	N "github.com/sagernet/sing/common/network"
 	"github.com/sagernet/sing/common/ntp"
