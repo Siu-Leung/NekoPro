58dcd53a8f869d81ef172651fa531a316d4afe8b
Siu-Leung <siu1874hk@gmail.com>
2026-09-11 10:18:00 +0800

    fix: use literal constants as DataStore speedTest defaults

--- NekoBox/app/src/main/java/io/nekohasekai/sagernet/database/DataStore.kt
+++ NekoBox/app/src/main/java/io/nekohasekai/sagernet/database/DataStore.kt
@@ -161,21 +161,11 @@ object DataStore : OnPreferenceDataStoreChangeListener {
     var appendHttpProxy by configurationStore.boolean(Key.APPEND_HTTP_PROXY)
     var connectionTestURL by configurationStore.string(Key.CONNECTION_TEST_URL) { CONNECTION_TEST_URL }
     var connectionTestConcurrent by configurationStore.int("connectionTestConcurrent") { 5 }
-    var speedTestMode by configurationStore.string(Key.SPEED_TEST_MODE) {
-        SagerNet.application.getString(R.string.default_speed_test_mode)
-    }
-    var speedTestTimeoutMs by configurationStore.stringToInt(Key.SPEED_TEST_TIMEOUT_MS) {
-        SagerNet.application.getString(R.string.default_speed_test_timeout_ms).toInt()
-    }
-    var speedTestServerListURL by configurationStore.string(Key.SPEED_TEST_SERVER_LIST_URL) {
-        SagerNet.application.getString(R.string.default_speed_test_server_list_url)
-    }
-    var speedTestFallbackServerListURL by configurationStore.string(Key.SPEED_TEST_FALLBACK_SERVER_LIST_URL) {
-        SagerNet.application.getString(R.string.default_speed_test_fallback_server_list_url)
-    }
-    var simpleDownloadURL by configurationStore.string(Key.SIMPLE_DOWNLOAD_URL) {
-        SagerNet.application.getString(R.string.default_simple_download_url)
-    }
+    var speedTestMode by configurationStore.string(Key.SPEED_TEST_MODE) { "download_upload" }
+    var speedTestTimeoutMs by configurationStore.stringToInt(Key.SPEED_TEST_TIMEOUT_MS) { 5000 }
+    var speedTestServerListURL by configurationStore.string(Key.SPEED_TEST_SERVER_LIST_URL) { "https://www.speedtest.net/api/js/servers" }
+    var speedTestFallbackServerListURL by configurationStore.string(Key.SPEED_TEST_FALLBACK_SERVER_LIST_URL) { "https://www.speedtest.net/speedtest-servers-static.php" }
+    var simpleDownloadURL by configurationStore.string(Key.SIMPLE_DOWNLOAD_URL) { "http://cachefly.cachefly.net/1mb.test" }
     var alwaysShowAddress by configurationStore.boolean(Key.ALWAYS_SHOW_ADDRESS)
 
     var tunImplementation by configurationStore.stringToInt(Key.TUN_IMPLEMENTATION) { TunImplementation.GVISOR }
